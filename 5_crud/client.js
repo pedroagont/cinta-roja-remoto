25e9d4ae27d77aeb316a5e02057c0c51540adc61
Pedro Gonzalez <pgonzalez@post.com>
2020-04-04 04:53:58 -0500

    Corregí detalles de ejercicio CRUD

--- 5_crud/client.js
+++ 5_crud/client.js
@@ -1,9 +1,145 @@
-const request = require('request');
-const URL_BASE = 'https://goodreads-devf-aaron.herokuapp.com/';
+// const request = require('request');
+// const URL_BASE = 'https://goodreads-devf-aaron.herokuapp.com/api/v1/authors/';
+//
+//  // Create - POST
+// const createAuthor = (name, last_name, nacionalidad, biography, gender, age) => {
+//
+//   const jsonSend = {
+//     'name': name,
+//     'last_name': last_name,
+//     'nacionalidad': nacionalidad,
+//     'biography': biography,
+//     'gender': gender,
+//     'age': age
+//   }
+//
+//   request.post(URL_BASE, { form: jsonSend }, (err, res, body) => {
+//     if(res.statusCode === 201){
+//       console.log(JSON.parse(body));
+//     } else console.log(res.statusCode, err);  })
+// }
+//
+// // createAuthor('Pedro', 'González', 'MX', 'Chavito bien', 'M', 26);
+//
+//
+//  // Read   - GET
+// // Para leer todos:
+// const readAllAuthors = (id) => {
+//   request.get(URL_BASE, (err, res, body) => {
+//     if(res.statusCode === 200){
+//       console.log(JSON.parse(body));
+//     } else console.log(res.statusCode, err);
+//   })
+// }
+//
+// // readAllAuthors()
+//
+//
+// // Para leer un solo autor
+// const readOneAuthor = (id) => {
+//   const URL = `${URL_BASE}${id}/`;
+//   request.get(URL, (err, res, body) => {
+//     if(res.statusCode === 200){
+//       console.log(JSON.parse(body));
+//     } else console.log(res.statusCode, err);
+//   })
+// }
+//
+// // readOneAuthor(6234)
+//
+//
+//  // Update - PUT/PATCH
+// const updateAuthor = (id, name, last_name, nacionalidad, biography, gender, age) => {
+//   const URL = `${URL_BASE}${id}/`;
+//
+//   const jsonSend = {
+//     'name': name,
+//     'last_name': last_name,
+//     'nacionalidad': nacionalidad,
+//     'biography': biography,
+//     'gender': gender,
+//     'age': age
+//   };
+//
+//   request.put(URL, { form: jsonSend }, (err, res, body) => {
+//     if(res.statusCode === 200){
+//       console.log(JSON.parse(body));
+//     } else console.log(res.statusCode, err);
+//   });
+// }
+//
+// // updateAuthor(6234, "Pedrito", "Pacheco", "USA", "Chavito good", "M", 10);
+//
+//
+//  // Delete - DELETE
+// const deleteAuthor = (id) => {
+//   const URL = `${URL_BASE}${id}/`;
+//   request.delete(URL, (err, res, body) => {
+//     if (res.statusCode === 204) console.log(`Id: ${id} fue eliminado correctamente`);
+//       // console.log(JSON.parse(body)); //El body regresa vacío, no es necesario parsearlo
+//     else console.log(res.statusCode, JSON.parse(body));
+//   });
+// }
+//
+// deleteAuthor(6134)
 
-//  Create - POST
+// EJERCICIO
+// Ejecutar las siguientes funciones:
+// Crear un autor
+// Modificar ese autor
+// Obtener autor
+// Borrar autor
+
+const request = require('request');
+const URL_BASE = 'https://goodreads-devf-aaron.herokuapp.com/api/v1/authors/';
+
+ // Delete - DELETE
+const deleteAuthor = (id) => {
+  const URL = `${URL_BASE}${id}/`;
+  request.delete(URL, (err, res, body) => {
+    console.log('Borrando autor...');
+    if (res.statusCode === 204) console.log(`id: ${id} fue eliminado correctamente`);
+      // console.log(JSON.parse(body)); //El body regresa vacío, no es necesario parsearlo
+    else console.log(res.statusCode, JSON.parse(body));
+  });
+}
+
+// Read   - GET
+const readOneAuthor = (id) => {
+ const URL = `${URL_BASE}${id}/`;
+ request.get(URL, (err, res, body) => {
+   console.log('Obteniendo autor...');
+   if(res.statusCode === 200){
+     console.log(JSON.parse(body));
+     deleteAuthor(JSON.parse(body).id)
+   } else console.log(res.statusCode, err);
+ })
+}
+
+// Update - PUT/PATCH
+const updateAuthor = (id, name, last_name, nacionalidad, biography, gender, age) => {
+ const URL = `${URL_BASE}${id}/`;
+
+ const jsonSend = {
+   'name': name,
+   'last_name': last_name,
+   'nacionalidad': nacionalidad,
+   'biography': biography,
+   'gender': gender,
+   'age': age
+ };
+
+ request.put(URL, { form: jsonSend }, (err, res, body) => {
+   console.log('Modificando autor...');
+   if(res.statusCode === 200){
+     console.log(JSON.parse(body));
+     readOneAuthor(JSON.parse(body).id);
+   } else console.log(res.statusCode, err);
+ });
+}
+
+ // Create - POST
 const createAuthor = (name, last_name, nacionalidad, biography, gender, age) => {
-  const URL = `${URL_BASE}/api/v1/authors/`;
 
   const jsonSend = {
     'name': name,
@@ -14,73 +150,12 @@ const createAuthor = (name, last_name, nacionalidad, biography, gender, age) => 
     'age': age
   }
 
-  request.post(URL, { form: jsonSend }, (err, res, body) => {
-    console.log(res.statusCode, JSON.parse(body));
-  })
-}
-
-// createAuthor('Pedro', 'González', 'MX', 'Chavo cool de Cancún', 'M', 26);
-
-
- // Read   - GET
-// Para leer todos:
-const readAllAuthors = (id) => {
-  const URL = `${URL_BASE}/api/v1/authors/`;
-
-  request.get(URL, (err, res, body) => {
-    if(res.statusCode === 200){
+  request.post(URL_BASE, { form: jsonSend }, (err, res, body) => {
+    console.log('Creando autor...');
+    if(res.statusCode === 201){
       console.log(JSON.parse(body));
-    } else console.log(res.statusCode, err);
-  })
+      updateAuthor(JSON.parse(body).id, "Pedrito", "Pedrini", "USA", "Chavito good", "M", 10);
+    } else console.log(res.statusCode, err);  })
 }
 
-// readAllAuthors()
-
-
-// Para leer un solo autor
-const readOneAuthor = (id) => {
-  const URL = `${URL_BASE}/api/v1/authors/${id}/`;
-
-  request.get(URL, (err, res, body) => {
-    if(res.statusCode === 200){
-      console.log(JSON.parse(body));
-    } else console.log(res.statusCode, err);
-  })
-}
-
-// readOneAuthor(6129)
-
-
-//  Update - PUT/PATCH
-const updateAuthor = (id, name, last_name, nacionalidad, biography, gender, age) => {
-  const URL = `${URL_BASE}/api/v1/authors/${id}/`;
-
-  const jsonSend = {
-    'name': name,
-    'last_name': last_name,
-    'nacionalidad': nacionalidad,
-    'biography': biography,
-    'gender': gender,
-    'age': age
-  };
-
-  request.put(URL, { form: jsonSend }, (err, res, body) => {
-    console.log(res.statusCode);
-    console.log(JSON.parse(body));
-  });
-}
-
-// updateAuthor(6129, "Yo mero", "Pacheco", "USA", "Aclamado mexicano que nacio en USA", "M", 10);
-
-
-//  Delete - DELETE
-const deleteAuthor = (id) => {
-  const URL = `${URL_BASE}/api/v1/authors/${id}/`;
-  request.delete(URL, (err, res, body) => {
-    console.log(res.statusCode);
-    // console.log(JSON.parse(body)); No tiene sentido parsear una respuesta vacía...
-    if (res.statusCode === 204) console.log('Eliminado correctamente!');
-  });
-}
-
-// deleteAuthor(6129)
+createAuthor('Pedro', 'González', 'MX', 'Chavito bien', 'M', 26);
