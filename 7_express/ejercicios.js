57bb715eaa253314df78c4fae4ee6de3a9fbff26
Pedro Gonzalez <pgonzalez@post.com>
2020-04-07 00:50:31 -0500

    Clase y ejercicios usando servidor express

---
+++ 7_express/ejercicios.js
@@ -0,0 +1,103 @@
+// MODULES
+const express = require('express');
+const app = express();
+const port = 4200;
+
+// MIDDLEWARES:
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+//EJERCICIOS
+// 1.- Agrega un endpoint '/api/' que responda a
+//         una petición de tipo GET con un código de estado 200
+//         y el siguiente json:
+//                     {'mensaje':'hola mundo'}
+
+app.get('/api', (req, res) => {
+  res.status(200).json({mensaje: 'Hola Mundo'});
+});
+// INSMONIA
+// http://localhost:4200/api
+// {
+//   "mensaje": "Hola Mundo"
+// }
+
+
+// 2.- Agrega un endpoint '/api/suma' que responda a una
+//     petición de tipo GET con la suma de dos números que
+//     reciba mediante las querys num1 y num2. El servidor
+//     debe responder con un código de estado 200 y un json
+//     como el siguiente:
+//                    {'resultado': 7}
+
+app.get('/api/suma', (req, res) => {
+  // console.log(req.query);
+  // const num1 = req.query.num1;
+  // const num2 = req.query.num2;
+  // Desestructuración (Destructuring Es6)
+  const { num1, num2 } = req.query;
+  const resultado = parseInt(num1) + parseInt(num2);
+  return res.status(200).json({ resultado });
+});
+// INSMONIA
+// http://localhost:4200/api/suma?num1=10&num2=20
+// {
+//   "resultado": 30
+// }
+
+// 3.- Agrega un endpoint '/api/usuario/' que responda a
+//     una petición de tipo GET con el nombre que sea
+//     recibido a través de params. El servidor debe responder
+//     con un código de estado 200 y un json como este:
+//                {'usuario': 'Edwin'}
+
+app.get('/api/usuario/:nombre', (req, res) => {
+  // console.log(req.params);
+  return res.status(200).json({usuario: req.params.nombre});
+});
+// INSMONIA
+// http://localhost:4200/api/usuario/Jorge
+// {
+//   "usuario": "Jorge"
+// }
+
+// 4.- Agrega un endpoint '/api/swapi' que responda a una
+//     petición de tipo GET con el personaje solicitado de
+//                         https://swapi.co/
+//     El cliente debe mandar el número de personaje mediante
+//     params. La respuesta del servidor debe lucir algo así
+//               {'personaje': {
+//                      'name': 'Luke Skywalker',
+//                       ...,
+//               }}
+
+const request = require('request');
+
+app.get('/api/swapi/:id', (req, res) => {
+  // console.log(req.params);
+  const { id } = req.params;
+  const URL_SWAPI = `https://swapi.co/api/people/${id}`
+  request.get(URL_SWAPI, (error, response, body) =>{
+    return response.statusCode === 200
+    ? res.json({personaje: JSON.parse(body)})
+    : res.json({message: 'Character not found...'});
+  });
+});
+// INSMONIA
+// http://localhost:4200/api/swapi/1
+// {
+//   "personaje": {
+//     "name": "Luke Skywalker",
+//      ...,
+//   }
+// }
+// http://localhost:4200/api/swapi/13
+// {
+//   "personaje": {
+//     "name": "Chewbacca",
+//      ...,
+//   }
+// }
+
+//LISTEN
+app.listen(port, () => console.log(`Listening @ http://localhost:${port}`));
---
+++ 7_express/server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Para no tener que cerrar proceso y volver a correrlo en caso de ser modificado
+// Usamos nodemon: sudo npm install nodemon --global
+
+// MIDDLEWARES:
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+// ENDPOINTS
+app.get('/', (req, res) => res.send('Bienvenido a mi servidor!'));
+app.get('/prueba', (req, res) => res.status(203).send({ message: 'This is a test'}));
+
+// ¿Cómo trabajamos con querys?
+app.get('/ejemplo', (req, res) => {
+  console.log('✅', req.query);
+  return res.send(req.query);
+});
+//INSOMNIA GET localhost:3000/ejemplo?precio=5000-12000&marca=Tohiba&fecha=2020-04-06
+
+// ¿Cómo trabajamos con params?
+app.put('/authors/:name/:age', (req, res) => {
+  console.log(req.params);
+  return res.status(200).send(req.params);
+});
+//INSOMNIA PUT localhost:3000/authors/1234/26
+
+// ¿Cómo trabajamos con el body?
+app.post('/profile', (req, res) => {
+  console.log(req.body)
+  // Aquí iríamos a la base de datos para
+  // intentar crear un elemento con lo que contiene req.body
+  const respuesta = { id: 57, ...req.body };
+  // Spread Syntax ...req.body: https://medium.com/@paulryan17/spread-operator-javascript-a77a890b96eb
+  res.status(201).json(respuesta);
+});
+//INSOMNIA POST localhost:3000/profile
+// {
+//   name: 'Peter',
+//   number: 99884554
+// }
+// {
+//   id: 57,
+//   name: 'Peter',
+//   number: 99884554
+// }
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
